ed22f9fafcd1083b50e917a063f1664a21934640
amanda-thrive <amanda@thrivemg.com>
2014-02-11 05:44:33 -0600

    Initialize rails: Can't initialize a new Rails application within the directory of another, please change to a non-Rails directory first. Type 'rails' for help.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Squeeker::Application.config.secret_key_base = 'd5366f2321ea52cc95cf9a3e9dcdcec727f2cf6e9ab5333a9d5e5938bf2146109a5ddad2aaf128fe992d45479e9b48ca6a245cae9c5d8509c37c657f093ed070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Squeeker::Application.config.session_store :cookie_store, key: '_squeeker_session'
